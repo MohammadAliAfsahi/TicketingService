9ffba12a4e0415c24b2f51b12ac1a05d0347d617
root <root@localhost.localdomain>
2019-03-30 13:56:13 -0400

    Changes in loging Class

--- server/server.py
+++ server/server.py
@@ -60,7 +60,7 @@ class BaseHandler(tornado.web.RequestHandler):
             return False
 
     def check_api(self,api):
-        resuser = self.db.get("SELECT * from user where api = %s", api)
+        resuser = self.db.get("SELECT * from user where apitoken = %s", api)
         if resuser:
             return True
         else:
@@ -81,7 +81,6 @@ class defaulthandler(BaseHandler):
         output = {'status':'Wrong Command'}
         self.write(output)
 
-
 class signup(BaseHandler):
     def get(self,*args):
         if not self.check_user(args[0]):
@@ -90,8 +89,11 @@ class signup(BaseHandler):
                                      "values (%s,%s,%s,%s,%s) "
                                      , args[0],args[1],args[2],args[3],api_token)
 
-            output = {'api': api_token,
-                      'status': 'OK'}
+            output = {
+                        "message": "Signed Up Successfully",
+                        "code": "200"
+                    }
+
             self.write(output)
         else:
             output = {'status': 'User Exist'}
@@ -107,18 +109,80 @@ class signup(BaseHandler):
                                      "values (%s,%s,%s,%s,%s) "
                                      , username,password,firstname,lastname,api_token)
 
-            output = {'api' : api_token,
-                      'status' : 'OK'}
+            output = {
+                        "message": "Signed Up Successfully",
+                        "code": "200"
+                    }
             self.write(output)
         else:
             output = {'status': 'User Exist'}
             self.write(output)
 
+class login(BaseHandler):
+    def get(self, *args, **kwargs):
+        if self.check_auth(args[0],args[1]):
+            api_token = str(hexlify(os.urandom(16)))
+            self.db.execute("UPDATE user set apitoken=%s where username=%s and password = %s",api_token,args[0],args[1])
+            user = self.db.get("SELECT * from user where username = %s and password = %s", args[0], args[1])
+            output = {
+                    "message": "Logged in Successfully",
+                    "code": "200",
+                     "api" : user.apitoken,
+                    }
+            self.write(output)
+        else:
+            output = {'status': 'FALSE'}
+            self.write(output)
+
+    def post(self, *args, **kwargs):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        if self.check_auth(username,password):
+            api_token = str(hexlify(os.urandom(16)))
+            self.db.execute("UPDATE user set apitoken=%s where username=%s and password = %s",api_token,args[0],args[1])
+            user = self.db.get("SELECT * from user where username = %s and password = %s", username, password)
+            output = {
+                    "message": "Logged in Successfully",
+                    "code": "200",
+                     "api" : user.apitoken,
+                    }
+            self.write(output)
+        else:
+            output = {'status': 'FALSE'}
+            self.write(output)
+
+class logout(BaseHandler):
+    def get(self, *args, **kwargs):
+        if self.check_auth(args[0],args[1]):
+            self.db.execute("UPDATE user set apitoken=%s where username=%s and password = %s"," ",args[0],args[1])
+            output = {
+                        "message": "Logged Out Successfully",
+                        "code": "200"
+                    }
+            self.write(output)
+        else:
+            output = {'status': 'FALSE'}
+            self.write(output)
+
+    def post(self, *args, **kwargs):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        if self.check_auth(username,password):
+            self.db.execute("UPDATE user set apitoken=%s where username=%s and password = %s"," ",username,password)
+            output = {
+                        "message": "Logged Out Successfully",
+                        "code": "200"
+                        }
+            self.write(output)
+        else:
+            output = {'status': 'FALSE'}
+            self.write(output)
+
 class getTicket(BaseHandler):
     def get(self,*args):
         if self.check_api(args[0]):
-            user = self.db.get("SELECT * from user where api = %s",args[0])
-            output = {'API' : user.api,
+            user = self.db.get("SELECT * from user where apitoken = %s",args[0])
+            output = {'API' : user.apitoken,
                       'Command': 'Balance',
                 'Username' : user.username,
                       'Balance' : user.balance
@@ -130,8 +194,8 @@ class getTicket(BaseHandler):
     def post(self, *args, **kwargs):
         api_token=self.get_argument('api')
         if self.check_api(api_token):
-            user = self.db.get("SELECT * from user where api = %s",api_token)
-            output = {'API' : user.api,
+            user = self.db.get("SELECT * from user where apitoken = %s",api_token)
+            output = {'API' : user.apitoken,
                       'Command': 'Balance',
                 'Username' : user.username,
                       'Balance' : user.balance
@@ -144,7 +208,7 @@ class GetTicket(BaseHandler):
     def get(self,*args):
         if self.check_auth(args[0],args[1]):
             user = self.db.get("SELECT * from user where username = %s and password = %s", args[0],args[1])
-            output = {'API' : user.api,
+            output = {'API' : user.apitoken,
                       'Command' : 'Balance',
                 'Username' : user.username,
                       'Balance' : user.balance}
@@ -157,7 +221,7 @@ class GetTicket(BaseHandler):
         password = self.get_argument('password')
         if self.check_auth(username,password):
             user = self.db.get("SELECT * from user where username = %s and password = %s", username,password)
-            output = {'API' : user.api,
+            output = {'API' : user.apitoken,
                       'Command' : 'Balance',
                 'Username' : user.username,
                       'Balance' : user.balance}
@@ -170,9 +234,9 @@ class GetTicket(BaseHandler):
 class apideposit(BaseHandler):
     def get(self,*args):
         if self.check_api(args[0]):
-            user_old = self.db.get("SELECT * from user where api = %s", args[0])
-            self.db.execute("UPDATE user set balance = balance + %s where api = %s",int(args[1]),args[0])
-            user_new = self.db.get("SELECT * from user where api = %s", args[0])
+            user_old = self.db.get("SELECT * from user where apitoken = %s", args[0])
+            self.db.execute("UPDATE user set balance = balance + %s where apitoken = %s",int(args[1]),args[0])
+            user_new = self.db.get("SELECT * from user where apitoken = %s", args[0])
             output = {'API': user_new.api,
                       'Command' : 'Deposit',
                       'Username': user_new.username,
@@ -187,9 +251,9 @@ class apideposit(BaseHandler):
         api_token = self.get_argument('api')
         amount = self.get_argument('amount')
         if self.check_api(api_token):
-            user_old = self.db.get("SELECT * from user where api = %s", api_token)
-            self.db.execute("UPDATE user set balance = balance + %s where api = %s",int(amount),api_token)
-            user_new = self.db.get("SELECT * from user where api = %s", api_token)
+            user_old = self.db.get("SELECT * from user where apitoken = %s", api_token)
+            self.db.execute("UPDATE user set balance = balance + %s where apitoken = %s",int(amount),api_token)
+            user_new = self.db.get("SELECT * from user where apitoken = %s", api_token)
             output = {'API': user_new.api,
                       'Command' : 'Deposit',
                       'Username': user_new.username,
@@ -199,7 +263,6 @@ class apideposit(BaseHandler):
         else:
             output = {'status': 'Wrong API'}
             self.write(output)
-
 
 class ChangeStatus(BaseHandler):
     def get(self, *args):
@@ -235,19 +298,18 @@ class ChangeStatus(BaseHandler):
             output = {'status': 'Wrong Authentication'}
             self.write(output)
 
-
 class apiwithdraw(BaseHandler):
     def get(self,*args):
         if self.check_api(args[0]):
-            user_old = self.db.get("SELECT * from user where api = %s", args[0])
+            user_old = self.db.get("SELECT * from user where apitoken = %s", args[0])
             if int(args[1]) > user_old.balance :
                 print(args[1],user_old.balance)
                 output = {'status': 'Insufficient Balance'}
                 self.write(output)
                 return
 
-            self.db.execute("UPDATE user set balance = balance - %s where api = %s",int(args[1]),args[0])
-            user_new = self.db.get("SELECT * from user where api = %s", args[0])
+            self.db.execute("UPDATE user set balance = balance - %s where apitoken = %s",int(args[1]),args[0])
+            user_new = self.db.get("SELECT * from user where apitoken = %s", args[0])
             output = {'API': user_new.api,
                       'Command' : 'Withdraw',
                       'Username': user_new.username,
@@ -263,9 +325,9 @@ class apiwithdraw(BaseHandler):
         api_token = self.get_argument('api')
         amount = self.get_argument('amount')
         if self.check_api(api_token):
-            user_old = self.db.get("SELECT * from user where api = %s", api_token)
-            self.db.execute("UPDATE user set balance = balance - %s where api = %s",int(amount),api_token)
-            user_new = self.db.get("SELECT * from user where api = %s", api_token)
+            user_old = self.db.get("SELECT * from user where apitoken = %s", api_token)
+            self.db.execute("UPDATE user set balance = balance - %s where apitoken = %s",int(amount),api_token)
+            user_new = self.db.get("SELECT * from user where apitoken = %s", api_token)
             output = {'API': user_new.api,
                       'Command' : 'Withdraw',
                       'Username': user_new.username,
@@ -323,62 +385,9 @@ class SendTicket(BaseHandler):
             output = {'status': 'Wrong Authentication'}
             self.write(output)
 
-
 class help(BaseHandler):
     def get(self, *args, **kwargs):
        self.write("Tornado is Runnig")
-
-
-class login(BaseHandler):
-    def get(self, *args, **kwargs):
-        if self.check_auth(args[0],args[1]):
-            user = self.db.get("SELECT * from user where username = %s and password = %s", args[0], args[1])
-            output = {'status' : 'TRUE',
-                      'api' : user.api,
-                      'username' : user.username}
-            self.write(output)
-        else:
-            output = {'status': 'FALSE'}
-            self.write(output)
-
-    def post(self, *args, **kwargs):
-        username = self.get_argument('username')
-        password = self.get_argument('password')
-        if self.check_auth(username,password):
-            user = self.db.get("SELECT * from user where username = %s and password = %s", username, password)
-            output = {'status': 'TRUE',
-                      'api': user.api,
-                      'username': user.username}
-            self.write(output)
-        else:
-            output = {'status': 'FALSE'}
-            self.write(output)
-
-
-class logout(BaseHandler):
-    def get(self, *args, **kwargs):
-        if self.check_api(args[0]):
-            user = self.db.get("SELECT * from user where api = %s", args[0])
-            output = {'status': 'TRUE',
-                      'api': user.api,
-                      'username': user.username}
-            self.write(output)
-        else:
-            output = {'status': 'FALSE'}
-            self.write(output)
-
-    def post(self, *args, **kwargs):
-        api = self.get_argument('api')
-        if self.check_api(api):
-            user = self.db.get("SELECT * from user where api = %s", api)
-            output = {'status': 'TRUE',
-                      'api': user.api,
-                      'username': user.username}
-            self.write(output)
-        else:
-            output = {'status': 'FALSE'}
-            self.write(output)
-
 
 def main():
     tornado.options.parse_command_line()
